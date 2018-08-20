a2b9ae25815520162584ad16cc0e14842b6dd4bd
Taisuke Yasuda <taisukey@andrew.cmu.edu>
2018-08-20 13:08:52 -0400

    update coursework

--- src/components/coursework/cmu.jsx
+++ src/components/coursework/cmu.jsx
@@ -6,11 +6,14 @@ const Semester = ({ sem }) => (
     <h3>{ sem.sem } { sem.year }</h3>
     <table>
       <tbody>
-        {
+        { sem.courses &&
           sem.courses.map((course, i) => (
             <tr key={ i }>
               <td>{ course.number }</td>
-              <td>{ course.name }</td>
+              <td>{
+                course.url ? (
+                  <a href={ course.url }>{ course.name }</a>
+                ) : course.name } { course.graduate && '*' }</td>
             </tr>
           ))
         }
@@ -20,26 +23,33 @@ const Semester = ({ sem }) => (
 );
 
 const CMU = () => (
-  <table>
-    <tbody style={ { verticalAlign: 'top' } }>
-      <tr>
-        <td><Semester sem={ cmu.spring18 } /></td>
-        <td><Semester sem={ cmu.fall17 } /></td>
-      </tr>
-      <tr>
-        <td><Semester sem={ cmu.spring17 } /></td>
-        <td><Semester sem={ cmu.fall16 } /></td>
-      </tr>
-      <tr>
-        <td><Semester sem={ cmu.sumII16 } /></td>
-        <td><Semester sem={ cmu.sumI16 } /></td>
-      </tr>
-      <tr>
-        <td><Semester sem={ cmu.spring16 } /></td>
-        <td><Semester sem={ cmu.fall15 } /></td>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <p>(* indicates graduate level)</p>
+    <table>
+      <tbody style={ { verticalAlign: 'top' } }>
+        <tr>
+          <td><Semester sem={ cmu.spring19 } /></td>
+          <td><Semester sem={ cmu.fall18 } /></td>
+        </tr>
+        <tr>
+          <td><Semester sem={ cmu.spring18 } /></td>
+          <td><Semester sem={ cmu.fall17 } /></td>
+        </tr>
+        <tr>
+          <td><Semester sem={ cmu.spring17 } /></td>
+          <td><Semester sem={ cmu.fall16 } /></td>
+        </tr>
+        <tr>
+          <td><Semester sem={ cmu.sumII16 } /></td>
+          <td><Semester sem={ cmu.sumI16 } /></td>
+        </tr>
+        <tr>
+          <td><Semester sem={ cmu.spring16 } /></td>
+          <td><Semester sem={ cmu.fall15 } /></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 );
 
 export default CMU;
